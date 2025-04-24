04f1f1d0e50f047607f1a67d97409e6871ccbb35
wen2ms <1819509111@qq.com>
2025-04-24 18:02:12 +0800

    Feat: init epoll and accpet connections

--- server.c
+++ server.c
@@ -1,17 +1,19 @@
 #include "server.h"
 
-#include "stdio.h"
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
 
 int init_listen_fd(unsigned short port) {
-    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_fd == -1) {
+    int lfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (lfd == -1) {
         perror("socket");
         return -1;
     }
 
     int opt = 1;
-    int ret = setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    int ret = setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     if (ret == -1) {
         perror("setsocketpot");
         return -1;
@@ -21,17 +23,73 @@ int init_listen_fd(unsigned short port) {
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
-    ret = bind(listen_fd, (struct sockaddr*)&addr, sizeof(addr));
+    ret = bind(lfd, (struct sockaddr*)&addr, sizeof(addr));
     if (ret == -1) {
         perror("bind");
         return -1;
     }
 
-    ret = listen(listen_fd, 128);
+    ret = listen(lfd, 128);
     if (ret == -1) {
         perror("listen");
         return -1;
     }
 
-    return listen_fd;
+    return lfd;
+}
+
+int epoll_run(int lfd) {
+    int epfd = epoll_create(1);
+    if (epfd == -1) {
+        perror("epoll_create");
+        return -1;
+    }
+
+    struct epoll_event event;
+    event.data.fd = lfd;
+    event.events = EPOLLIN;
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &event);
+    if (ret == -1) {
+        perror("epoll_ctl");
+        return -1;
+    }
+
+    struct epoll_event events[1024];
+    int size = sizeof(events) / sizeof(struct epoll_event);
+    while (1) {
+        int num = epoll_wait(epfd, events, size, -1);
+        for (int i = 0; i < num; ++i) {
+            int fd = events[i].data.fd;
+            if (fd == lfd) {
+                accept_client(lfd, epfd);
+            } else {
+                
+            }
+        }
+    }
+
+    return 0;
+}
+
+int accept_client(int lfd, int epfd) {
+    int cfd = accept(lfd, NULL, NULL);
+    if (cfd == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    int flag = fcntl(cfd, F_GETFL);
+    flag |= O_NONBLOCK;
+    fcntl(cfd, F_SETFL, flag);
+
+    struct epoll_event event;
+    event.data.fd = cfd;
+    event.events = EPOLLIN | EPOLLET;
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &event);
+    if (ret == -1) {
+        perror("epoll_ctl");
+        return -1;
+    }
+
+    return 0;
 }
--- server.h
+++ server.h
@@ -1,3 +1,7 @@
 #pragma once
 
 int init_listen_fd(unsigned short port);
+
+int epoll_run(int lfd);
+
+int accept_client(int lfd, int epfd);
